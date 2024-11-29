59628128021bb79bdb372b28bc5f5e594f5ff834
yama-chiiii <nagayama0409@gmail.com>
2024-11-29 14:11:08 +0900

    コード書きはじめ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="font-mPlus">
-      <div className="w-1/2 h-screen bg-white text-2xl">こんにちは</div>
+    <div className="w-full min-h-screen flex flex-col items-center bg-blue-100">
+      <div className="w-1/2 h-screen bg-white text-2xl font-mPlus">
+        <div className="mt-64 mx-36 font-semibold border-b-4">今日の記録</div>
+      </div>
     </div>
   );
 }
